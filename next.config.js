580055799fa91af1b0ccfdd7c912bd5418170f90
Deon James <dad@Carissas-MacBook-Air.local>
2025-12-25 10:53:28 -0500

    Serve AASA with correct Content-Type

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  async headers() {
+    return [
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [
+          { key: "Content-Type", value: "application/json" },
+          // Optional but nice:
+          { key: "Cache-Control", value: "public, max-age=300" },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
+
